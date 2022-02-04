b4d3bb5d8ba3585beb8453c8aa9056e086b7c734
Mariana Gonzalez <mariana.gonzalez@wizeline.com>
2022-02-03 18:25:58 -0600

    note taker complete version

---
+++ server.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+// Helper method for generating unique id's
+const uuid = require("./helpers/uuid");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+
+// GET request for notes
+app.get("/api/notes", (req, res) => {
+  // Obtain existing notes
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // Convert string into JSON object
+      const parsedNotes = JSON.parse(data);
+
+      res.send(JSON.stringify(parsedNotes));
+    }
+  });
+});
+
+// DELETE request for a note
+app.delete("/api/notes/:term", (req, res) => {
+  const requestedNote = req.params.term;
+  // Obtain existing notes
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // Convert string into JSON object
+      const parsedNotes = JSON.parse(data);
+      // delete note
+      const i = parsedNotes.findIndex((note) => {
+        return note.id === requestedNote;
+      });
+      parsedNotes.splice(i, 1);
+      // Write updated notes back to the file
+      fs.writeFile(
+        "./db/db.json",
+        JSON.stringify(parsedNotes, null, 4),
+        (writeErr) =>
+          writeErr ? console.error(writeErr) : console.info("Notes update")
+      );
+      res.status(200).json("Status 200");
+    }
+  });
+});
+
+// POST request to add a note
+app.post("/api/notes", (req, res) => {
+  // Log that a POST notes was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    // Obtain existing notes
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+        // Add a new note
+        parsedNotes.push(newNote);
+
+        // Write updated notes back to the file
+        fs.writeFile(
+          "./db/db.json",
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr ? console.error(writeErr) : console.info("Notes update")
+        );
+      }
+    });
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(200).json(response);
+  } else {
+    res.status(500).json("Error in posting note");
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
